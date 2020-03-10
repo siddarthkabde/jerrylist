3b3f9e1bc42095a1f5b91a0a384540019798c6ad
siddarthkabde <siddarth.k8055@gmail.com>
2020-03-10 14:24:42 +0000

    Version V2

--- local.js
+++ local.js
@@ -1,99 +1,52 @@
-var path = require('path');
-express = require('express');
+var path = require('path'),
+	express = require('express');
 var qs = require('querystring');
-var MongoClient = require('mongodb').MongoClient;
-var contactlistDb = require('./service/contactlistDb.js');
-
-var bodyParser = require('body-parser');
-
-var app = express( );
-var db;
-
-MongoClient.connect("mongodb://localhost:27017/contactlist", (err, database) => {
-	if (err) return console.log(err);
-
-	db = database;
-	app.use((req, res, next)=>{
-		req.db = db;
-		next();
-	});
-
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({extended: true}));
-	app.use(express.static(__dirname + '/webapp'));
-	app.use("/service/contactlistDb", contactlistDb);
-	
-	// Initialize the app.
-	var server = app.listen(process.env.PORT, function () {
-		var port = server.address().port;
-		console.log("App now running on port", port);
-	});
+var app = express();
+app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
+app.use('/wt', express.static(path.join(__dirname, 'walkthrough')));
+app.use('/mindmap', express.static(path.join(__dirname, 'mindmap')));
+app.use('/module', express.static(path.join(__dirname, 'module')));
+app.get('/', function (req, res) {
+	console.log("method in get/: " + req.method);
+	var qs = require('querystring');
+	res.send("Hello World");
 });
 
+app.post("/", function (req, res) {
+	var body = '';
+	const regex = /!\[(.*?)\]\((.*?)\)/g;
+	var m;
+	var printResult = (array) => {
+		var aResult = [];
+		var url = array[2];
+		var splited = url.split(".");
+		var oResult = {
+			"localFile": array[1] + "." + splited[splited.length - 1],
+			"fileUrl": url
+		};
+		aResult.push(oResult);
+		return aResult;
+	};
+	req.on('data', function (data) {
+		body += data;
+		if (body.length > 1e6)
+			request.connection.destroy();
+	});
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-// var db;
-
-// Connect to the database before starting the application server.
-// mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, client) {
-// 	if (err) {
-// 		console.log(err);
-// 		process.exit(1);
-// 	}
-
-// 	// Save database object from the callback for reuse.
-// 	db = client.db();
-// 	console.log("Database connection ready");
-
-// 	// Initialize the app.
-// 	var server = app.listen(process.env.PORT, function () {
-// 		var port = server.address().port;
-// 		console.log("App now running on port", port);
-// 	});
-// });
-app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
-// app.get('/', function(req, res){
-// 	console.log("method: " + req.method);
-//   res.send("Hello World");
-// });
-
-// app.get('/loyoladb', emp.findAll);
-
-// app.post("/", function(req, res){
-// 	var body = '';
-// 	const regex = /!\[(.*?)\]\((.*?)\)/g;
-// 	var m;
-// 	var printResult = ( array ) => {
-// 		var aResult = [];
-//     	var url = array[2];
-//     	var splited = url.split(".");
-//     	var oResult = {
-//     		"localFile": array[1] + "." + splited[splited.length-1],
-//     		"fileUrl": url
-//     	};
-// 		aResult.push(oResult);
-// 		return aResult;
-// 	};
-// 	req.on('data', function (data) {
-//             body += data;
-//             if (body.length > 1e6)
-//                 request.connection.destroy();
-//         });
-
-//     req.on('end', function () {
-//             var post = qs.parse(body);
-//             var aResult = [];
-//             // res.send("your request is: " + post.markdown_source);
-//             while ((m = regex.exec(post.markdown_source)) !== null) {
-//     			if (m.index === regex.lastIndex) {
-//         			regex.lastIndex++;
-//     			}
-//     			aResult = aResult.concat(printResult(m));
-//     		}
-//     		console.log(aResult);
-//     		res.json(aResult);
-//     	});
-// 	});
-// app.listen(process.env.PORT || 3000, function(){
-//      console.log("Example app listens on port 3000.");
-// });
+	req.on('end', function () {
+		var post = qs.parse(body);
+		var aResult = [];
+		// res.send("your request is: " + post.markdown_source);
+		while ((m = regex.exec(post.markdown_source)) !== null) {
+			if (m.index === regex.lastIndex) {
+				regex.lastIndex++;
+			}
+			aResult = aResult.concat(printResult(m));
+		}
+		console.log(aResult);
+		res.json(aResult);
+	});
+});
+app.listen(process.env.PORT || 3000, function () {
+	console.log("Example app listens on port 3000.");
+});
--- webapp/Component.js
+++ webapp/Component.js
@@ -1,12 +1,65 @@
+/*eslint linebreak-style: ["error", "unix"]*/
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
-	"ZIProfile/model/models"
-], function(UIComponent, Device, models) {
+	"jerrylist/model/models",
+	"sap/m/Button"
+], function(UIComponent, Device, models, Button) {
 	"use strict";
 
-	return UIComponent.extend("ZIProfile.Component", {
+	debugger;
 
+	var JerryButton = Button.extend("sap.m.custom.JerryButton", {
+      metadata: {
+         properties: {
+            application: {
+               type: "object",
+               group: "Data",
+               defaultValue: null
+            }
+         },
+         aggregations: {
+            groups: {
+               type: "sap.client.m.settings.SettingsGroup",
+               multiple: false,
+               singularName: "group"
+            },
+            content: {
+               type: "sap.ui.core.Control",
+               multiple: true
+            }
+         }
+      },
+      renderer: {}/*,
+      constructor: function(oRef) {
+         this.application = oRef.application;
+         Button.apply(this, arguments);
+      }*/
+   });
+
+   var oApplication = {
+   	"Jerry": "i042416"
+   }; 
+/*
+   JerryButton.prototype.constructor = function(oRef){
+		console.log("I am here");
+		debugger;
+	}; 
+	*/
+
+
+  var AnotherButton = function(oRef){
+		console.log("I am here");
+		debugger;
+	}; 
+
+  AnotherButton.prototype = Object.create(Button.prototype);
+  AnotherButton.prototype.constructor = function(){
+  	debugger;
+  }
+
+  var oJerryButton = new AnotherButton({ application: oApplication } );
+	return UIComponent.extend("jerrylist.Component", {
 		metadata: {
 			manifest: "json"
 		},
@@ -17,18 +70,11 @@ sap.ui.define([
 		 * @override
 		 */
 		init: function() {
-			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
-			// enable routing
-			var oRouter = this.getRouter();
-			if (oRouter) {
-				oRouter.initialize();
-			}
-
-			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
+			var oModel = new sap.ui.model.json.JSONModel(jQuery.sap.getModulePath("jerrylist.model","/mock.json"));
+			this.setModel(oModel);
 		}
 	});
-
 });
