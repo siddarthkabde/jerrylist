ff4d1920a6775c77e1274806f6b731a8ed05beab
siddarthkabde <siddarth.kabde@gmail.com>
2020-03-12 16:03:17 +0000

    V1

--- app.js
+++ app.js
@@ -34,6 +34,19 @@
 
 //
 // Preamble
+
+var path = require('path'), express = require('express');
+var qs = require('querystring');
+var app = express();
+// app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
+// app.use('/wt', express.static(path.join(__dirname, 'walkthrough')));
+// app.use('/mindmap', express.static(path.join(__dirname, 'mindmap')));
+// app.use('/module', express.static(path.join(__dirname, 'module')));
+app.get('/', function(req, res){
+	console.log("method in get/: " + req.method);
+    var qs = require('querystring');
+   res.send("Hello World");
+});
 var http = require ('http');	     // For serving a basic web page.
 var mongoose = require ("mongoose"); // The reason for this demo.
 
@@ -57,110 +70,114 @@ mongoose.connect(uristring, function (err, res) {
   }
 });
 
+app.listen(process.env.PORT || 3000, function(){
+     console.log("Example app listens on port 3000.");
+});
+
 // This is the schema.  Note the types, validation and trim
-// statements.  They enforce useful constraints on the data.
-var userSchema = new mongoose.Schema({
-  name: {
-    first: String,
-    last: { type: String, trim: true }
-  },
-  age: { type: Number, min: 0}
-});
+// // statements.  They enforce useful constraints on the data.
+// var userSchema = new mongoose.Schema({
+//   name: {
+//     first: String,
+//     last: { type: String, trim: true }
+//   },
+//   age: { type: Number, min: 0}
+// });
 
-// Compiles the schema into a model, opening (or creating, if
-// nonexistent) the 'PowerUsers' collection in the MongoDB database
-var PUser = mongoose.model('PowerUsers', userSchema);
+// // Compiles the schema into a model, opening (or creating, if
+// // nonexistent) the 'PowerUsers' collection in the MongoDB database
+// var PUser = mongoose.model('PowerUsers', userSchema);
 
-// Clear out old data
-PUser.remove({}, function(err) {
-  if (err) {
-    console.log ('error deleting old data.');
-  }
-});
+// // Clear out old data
+// PUser.remove({}, function(err) {
+//   if (err) {
+//     console.log ('error deleting old data.');
+//   }
+// });
 
-// Creating one user.
-var johndoe = new PUser ({
-  name: { first: 'John', last: 'Doe' },
-  age: 25
-});
+// // Creating one user.
+// var johndoe = new PUser ({
+//   name: { first: 'John', last: 'Doe' },
+//   age: 25
+// });
 
-// Saving it to the database.  
-johndoe.save(function (err) {if (err) console.log ('Error on save!')});
+// // Saving it to the database.  
+// johndoe.save(function (err) {if (err) console.log ('Error on save!')});
 
-// Creating more users manually
-var janedoe = new PUser ({
-  name: { first: 'Jane', last: 'Doe' },
-  age: 65
-});
-janedoe.save(function (err) {if (err) console.log ('Error on save!')});
+// // Creating more users manually
+// var janedoe = new PUser ({
+//   name: { first: 'Jane', last: 'Doe' },
+//   age: 65
+// });
+// janedoe.save(function (err) {if (err) console.log ('Error on save!')});
 
-// Creating more users manually
-var alicesmith = new PUser ({
-  name: { first: 'Alice', last: 'Smith' },
-  age: 45
-});
-alicesmith.save(function (err) {if (err) console.log ('Error on save!')});
+// // Creating more users manually
+// var alicesmith = new PUser ({
+//   name: { first: 'Alice', last: 'Smith' },
+//   age: 45
+// });
+// alicesmith.save(function (err) {if (err) console.log ('Error on save!')});
 
 
-// In case the browser connects before the database is connected, the
-// user will see this message.
-var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
+// // In case the browser connects before the database is connected, the
+// // user will see this message.
+// var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
 
-// Create a rudimentary http server.  (Note, a real web application
-// would use a complete web framework and router like express.js). 
-// This is effectively the main interaction loop for the application. 
-// As new http requests arrive, the callback function gets invoked.
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  createWebpage(req, res);
-}).listen(theport);
+// // Create a rudimentary http server.  (Note, a real web application
+// // would use a complete web framework and router like express.js). 
+// // This is effectively the main interaction loop for the application. 
+// // As new http requests arrive, the callback function gets invoked.
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   createWebpage(req, res);
+// }).listen(theport);
 
-function createWebpage (req, res) {
-  // Let's find all the documents
-  PUser.find({}).exec(function(err, result) { 
-    if (!err) { 
-      res.write(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
-      // Let's see if there are any senior citizens (older than 64) with the last name Doe using the query constructor
-      var query = PUser.find({'name.last': 'Doe'}); // (ok in this example, it's all entries)
-      query.where('age').gt(64);
-      query.exec(function(err, result) {
-	if (!err) {
-	  res.end(html4 + JSON.stringify(result, undefined, 2) + html5 + result.length + html6);
-	} else {
-	  res.end('Error in second query. ' + err)
-	}
-      });
-    } else {
-      res.end('Error in first query. ' + err)
-    };
-  });
-}
+// function createWebpage (req, res) {
+//   // Let's find all the documents
+//   PUser.find({}).exec(function(err, result) { 
+//     if (!err) { 
+//       res.write(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
+//       // Let's see if there are any senior citizens (older than 64) with the last name Doe using the query constructor
+//       var query = PUser.find({'name.last': 'Doe'}); // (ok in this example, it's all entries)
+//       query.where('age').gt(64);
+//       query.exec(function(err, result) {
+// 	if (!err) {
+// 	  res.end(html4 + JSON.stringify(result, undefined, 2) + html5 + result.length + html6);
+// 	} else {
+// 	  res.end('Error in second query. ' + err)
+// 	}
+//       });
+//     } else {
+//       res.end('Error in first query. ' + err)
+//     };
+//   });
+// }
 
-// Tell the console we're getting ready.
-// The listener in http.createServer should still be active after these messages are emitted.
-console.log('http server will be listening on port %d', theport);
-console.log('CTRL+C to exit');
+// // Tell the console we're getting ready.
+// // The listener in http.createServer should still be active after these messages are emitted.
+// console.log('http server will be listening on port %d', theport);
+// console.log('CTRL+C to exit');
 
-//
-// House keeping.
+// //
+// // House keeping.
 
-//
-// The rudimentary HTML content in three pieces.
-var html1 = '<title> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </title> \
-<head> \
-<style> body {color: #394a5f; font-family: sans-serif} </style> \
-</head> \
-<body> \
-<h1> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </h1> \
-See the <a href="https://devcenter.heroku.com/articles/nodejs-mongoose">supporting article on the Dev Center</a> to learn more about data modeling with Mongoose. \
-<br\> \
-<br\> \
-<br\> <h2> All Documents in MonogoDB database </h2> <pre><code> ';
-var html2 = '</code></pre> <br\> <i>';
-var html3 = ' documents. </i> <br\> <br\>';
-var html4 = '<h2> Queried (name.last = "Doe", age >64) Documents in MonogoDB database </h2> <pre><code> ';
-var html5 = '</code></pre> <br\> <i>';
-var html6 = ' documents. </i> <br\> <br\> \
-<br\> <br\> <center><i> Demo code available at <a href="http://github.com/mongolab/hello-mongoose">github.com</a> </i></center>';
+// //
+// // The rudimentary HTML content in three pieces.
+// var html1 = '<title> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </title> \
+// <head> \
+// <style> body {color: #394a5f; font-family: sans-serif} </style> \
+// </head> \
+// <body> \
+// <h1> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </h1> \
+// See the <a href="https://devcenter.heroku.com/articles/nodejs-mongoose">supporting article on the Dev Center</a> to learn more about data modeling with Mongoose. \
+// <br\> \
+// <br\> \
+// <br\> <h2> All Documents in MonogoDB database </h2> <pre><code> ';
+// var html2 = '</code></pre> <br\> <i>';
+// var html3 = ' documents. </i> <br\> <br\>';
+// var html4 = '<h2> Queried (name.last = "Doe", age >64) Documents in MonogoDB database </h2> <pre><code> ';
+// var html5 = '</code></pre> <br\> <i>';
+// var html6 = ' documents. </i> <br\> <br\> \
+// <br\> <br\> <center><i> Demo code available at <a href="http://github.com/mongolab/hello-mongoose">github.com</a> </i></center>';
 
 
