fdcf44b8726518b204ac1d92f6b704e7c1da6dc2
siddarthkabde <siddarth.kabde@gmail.com>
2020-03-12 16:30:32 +0000

    V4

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,16 +1,24 @@
+/*eslint linebreak-style: ["error", "unix"]*/
 sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/Device"
-], function (JSONModel, Device) {
+], function(JSONModel, Device) {
 	"use strict";
 
 	return {
 
-		createDeviceModel: function () {
-			var oModel = new JSONModel(Device);
+		createDeviceModel: function() {
+			// Jerry 2017-05-20 9:32AM 
+			/*var oModel = new JSONModel(Device);
 			oModel.setDefaultBindingMode("OneWay");
-			return oModel;
+			return oModel;*/
+			var deviceModel = new sap.ui.model.json.JSONModel({
+			isPhone : jQuery.device.is.phone,
+			listMode : (jQuery.device.is.phone) ? "None" : "SingleSelectMaster",
+			listItemType : (jQuery.device.is.phone) ? "Active" : "Inactive"
+			});
+			deviceModel.setDefaultBindingMode("OneWay");
+			return deviceModel;
 		}
-
 	};
 });
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"com/heroku/HerokuDeploy/test/unit/controller/IntialPage.controller"
-], function () {
-	"use strict";
-});
