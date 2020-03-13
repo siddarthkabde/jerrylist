86574bb44c662788c583e0434bd05087f1418e50
siddarthkabde <siddarth.kabde@gmail.com>
2020-03-13 16:48:00 +0000

    V2

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var path = require('path'),
-	express = require('express');
+
+
+var path = require('path'), express = require('express');
 var qs = require('querystring');
 var app = express();
 
-var bodyParser = require('body-parser');
+
+var bodyParser= require('body-parser');
+
 
 // app.get('/', function(req, res){
 // 	console.log("method in get/: " + req.method);
@@ -14,18 +17,16 @@ var bodyParser = require('body-parser');
 app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-var http = require('http'); // For serving a basic web page.
-var mongoose = require("mongoose"); // The reason for this demo.
+var http = require ('http');	     // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.  
-var uristring =
-	process.env.MONGODB_URI ||
-	'mongodb://localhost/HelloMongoose';
+var uristring = 
+  process.env.MONGODB_URI || 
+  'mongodb://localhost/HelloMongoose';
 
 // The http server will listen to an appropriate port, or default to
 // port 5000.
@@ -36,25 +37,23 @@ var theport = process.env.PORT || 5000;
 
 var db
 mongoose.connect(uristring, function (err, client) {
-	if (err) {
-		console.log('ERROR connecting to: ' + uristring + '. ' + err);
-	} else {
-		console.log('Succeeded connected to: ' + uristring);
-		db = client;
-
-    });
-
-	}
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+    db = client;z
+  }
 });
 
-app.listen(process.env.PORT || 5000, function () {
-	console.log("Example app listens on port 3000.");
+app.listen(process.env.PORT || 5000, function(){
+     console.log("Example app listens on port 3000.");
 });
+
 
 app.get('/', (req, res) => {
-	db.collection('contactlist').find().toArray((err, results) => {
-		res.send(results)
-	});
+    db.collection('contactlist').find().toArray( (err, results) => {
+      res.send(results)
+    });
 });
 
 // app.post('/SaveStudentMaster', (req, res) => {
@@ -65,8 +64,15 @@ app.get('/', (req, res) => {
 //   });
 // })
 
+
 app.post('/SaveStudentMaster', (req, res) => {
-	//db.collection('StudentMaster').save(req.body, (err, result) => {});
-	var body = req.body;
-	db.collection('StudentMaster').insertOne(body);
+  //db.collection('StudentMaster').save(req.body, (err, result) => {});
+  var body = req.body;
+  db.collection('StudentMaster').insertOne(body);
 })
+
+
+
+
+
+
