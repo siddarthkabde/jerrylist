e63cb40dbcb42a291588ded22097406f487f8e0b
siddarthkabde <siddarth.kabde@gmail.com>
2020-03-28 12:48:11 +0000

    V1

--- webapp/Component.js
+++ webapp/Component.js
@@ -7,58 +7,6 @@ sap.ui.define([
 ], function(UIComponent, Device, models, Button) {
 	"use strict";
 
-	// debugger;
-
-	var JerryButton = Button.extend("sap.m.custom.JerryButton", {
-      metadata: {
-         properties: {
-            application: {
-               type: "object",
-               group: "Data",
-               defaultValue: null
-            }
-         },
-         aggregations: {
-            groups: {
-               type: "sap.client.m.settings.SettingsGroup",
-               multiple: false,
-               singularName: "group"
-            },
-            content: {
-               type: "sap.ui.core.Control",
-               multiple: true
-            }
-         }
-      },
-      renderer: {}/*,
-      constructor: function(oRef) {
-         this.application = oRef.application;
-         Button.apply(this, arguments);
-      }*/
-   });
-
-   var oApplication = {
-   	"Jerry": "i042416"
-   }; 
-/*
-   JerryButton.prototype.constructor = function(oRef){
-		console.log("I am here");
-		debugger;
-	}; 
-	*/
-
-
-  var AnotherButton = function(oRef){
-		console.log("I am here");
-		// debugger;
-	}; 
-
-  AnotherButton.prototype = Object.create(Button.prototype);
-  AnotherButton.prototype.constructor = function(){
-  	// debugger;
-  }
-
-  var oJerryButton = new AnotherButton({ application: oApplication } );
 	return UIComponent.extend("loyolabdn.Component", {
 		metadata: {
 			manifest: "json"
@@ -71,10 +19,8 @@ sap.ui.define([
 		 */
 		init: function() {
 			UIComponent.prototype.init.apply(this, arguments);
-
-			this.setModel(models.createDeviceModel(), "device");
-			var oModel = new sap.ui.model.json.JSONModel(jQuery.sap.getModulePath("loyolabdn.model","/mock.json"));
-			this.setModel(oModel);
+			// enable routing
+			this.getRouter().initialize();
 		}
 	});
 });
---
+++ webapp/controller/BaseController.js
@@ -0,0 +1,37 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
+	"sap/ui/core/UIComponent"
+], function (Controller, History, UIComponent) {
+	"use strict";
+
+	return Controller.extend("Loyola.controller.BaseController", {
+
+		getRouter: function () {
+			return UIComponent.getRouterFor(this);
+		},
+
+		onNavBack: function () {
+			var oHistory, sPreviousHash;
+
+			oHistory = History.getInstance();
+			sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getRouter().navTo("appHome", {}, true /*no history*/ );
+			}
+		},
+
+		getModel: function (sModel) {
+			return this.getOwnerComponent().getModel(sModel);
+		},
+
+		setModel: function (oModel, sName) {
+			return this.getOwnerComponent().setModel(oModel, sName);
+		},
+
+	});
+
+});
