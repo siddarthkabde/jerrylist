857e2ad2c1908420b29e56aee953fbf5f7087280
siddarthkabde <siddarth.kabde@gmail.com>
2020-03-31 16:11:25 +0000

    V2

--- webapp/controller/StudentTableView.controller.js
+++ webapp/controller/StudentTableView.controller.js
@@ -17,16 +17,7 @@ sap.ui.define([
 		// on Init method. 
 		onInit: function () {
 
-			var oRouter = this.getRouter();
-			oRouter.getRoute("employeeList").attachMatched(this._onRouteMatched, this);
-		},
-
-		// Method triggered when routing is matched. 
-		_onRouteMatched: function (oEvent) {
-			var oArgs, oView;
-			oArgs = oEvent.getParameter("arguments");
-			oView = this.getView();
-			var oStore1 = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+			// start of code
 			var oModel = new sap.ui.model.json.JSONModel();
 			oModel.loadData("/products/test", {}, false);
 			var oFinalResult = oModel.getData();
@@ -148,6 +139,139 @@ sap.ui.define([
 				this.oTPC.activate();
 			}
 
+	// end of code
+
+			var oRouter = this.getRouter();
+			oRouter.getRoute("employeeList").attachMatched(this._onRouteMatched, this);
+		},
+
+		// Method triggered when routing is matched. 
+		_onRouteMatched: function (oEvent) {
+			var oArgs, oView;
+			oArgs = oEvent.getParameter("arguments");
+			oView = this.getView();
+			var oStore1 = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+			// var oModel = new sap.ui.model.json.JSONModel();
+			// oModel.loadData("/products/test", {}, false);
+			// var oFinalResult = oModel.getData();
+			// oModel.setProperty("/", oFinalResult);
+			// var oModelData = oModel.getProperty("/");
+
+			// var oColumns = Object.keys(oModelData[0]);
+			// var oColumnNames = [];
+
+			// $.each(oColumns, function (i, value) {
+			// 	oColumnNames.push({
+			// 		Text: oColumns[i]
+			// 	});
+			// });
+
+			// oModel.setProperty("/columnNames", oColumnNames);
+			// var oTemplate = new Column({
+			// 	header: new Label({
+			// 		text: "{Text}"
+			// 	})
+			// });
+
+			// var oTable = this.byId("idProductsTable");
+			// oTable.setModel(oModel);
+			// oTable.bindAggregation("columns", "/columnNames", oTemplate);
+			// var oItemTemplate = new ColumnListItem();
+			// var oTableHeaders = oTable.getColumns("columns");
+			// // oTableHeaders = oModel.getProperty("/columnNames");
+			// var current = 0;
+			// $.each(oTableHeaders, function (j, value) {
+			// 	var oHeaderName = oTableHeaders[j].getHeader().getText();
+			// 	// var oHeaderName = oTableHeaders[j].Text;
+			// 	oItemTemplate.addCell(new Text({
+			// 		text: "{" + oHeaderName + "}"
+			// 	}));
+
+			// 	oItemTemplate.setType(sap.m.ListType.Navigation);
+			// 	// oItemTemplate.press("NavigateToStudentDetails");
+
+			// 	if (current < 4) {
+			// 		oTable.getColumns()[j].setVisible(true);
+			// 	} else {
+			// 		oTable.getColumns()[j].setVisible(false);
+			// 	}
+			// 	current++;
+			// });
+
+			// oTable.bindItems("/", oItemTemplate);
+			// this.oPersoService = {
+			// 	getPersData: function () {
+			// 		var oDeferred = new jQuery.Deferred();
+			// 		var oBundle = this._oBundle;
+			// 		oDeferred.resolve(oBundle);
+			// 		return oDeferred.promise();
+			// 	},
+
+			// 	setPersData: function (oBundle) {
+			// 		var oDeferred = new jQuery.Deferred();
+			// 		this._oBundle = oBundle;
+			// 		oDeferred.resolve();
+			// 		return oDeferred.promise();
+			// 	},
+
+			// 	resetPersData: function () {
+			// 		var oDeferred = new jQuery.Deferred();
+
+			// 		var oInitialData = {
+			// 			_persoSchemaVersion: "1.0",
+			// 			aColumns: [{
+			// 				id: "empty_component-testTable-LASTNAME",
+			// 				order: 2,
+			// 				text: "LASTNAME",
+			// 				visible: false
+			// 			}, {
+			// 				id: "empty_component-testTable-FIRSTNAME",
+			// 				order: 1,
+			// 				text: "FIRSTNAME",
+			// 				visible: true
+			// 			}, {
+			// 				id: "empty_component-testTable-EMPID",
+			// 				order: 0,
+			// 				text: "EMPID",
+			// 				visible: true
+			// 			}]
+			// 		};
+
+			// 		this._oBundle = oInitialData;
+			// 		oDeferred.resolve();
+			// 		return oDeferred.promise();
+			// 	},
+
+			// 	getCaption: function (oColumn) {
+			// 		if (oColumn.getHeader() && oColumn.getHeader().getText) {
+			// 			if (oColumn.getHeader().getText() == "Color") {
+			// 				return "Color: this is a very very very very long Column Name to check how the TablePersoDialog deals with it";
+			// 			}
+			// 		}
+			// 		return null;
+			// 	},
+
+			// 	getGroup: function (oColumn) {
+			// 		if (oColumn.getHeader() && oColumn.getHeader().getText) {
+			// 			if (oColumn.getHeader().getText() == "Color") {
+			// 				return "Primary Group";
+			// 			}
+			// 		}
+
+			// 		return "Secondary Group";
+			// 	}
+			// };
+			// // this.oPersoService.resetPersData();
+
+			// if (this.oTPC === undefined) {
+			// 	this.oTPC = new TablePersoController("idPersonalize", {
+			// 		table: oTable,
+			// 		persoService: this.oPersoService,
+			// 		hasGrouping: false
+			// 	});
+			// 	this.oTPC.activate();
+			// }
+
 		},
 
 		NavigateToStudentDetails: function (oEvent) {
@@ -173,7 +297,7 @@ sap.ui.define([
 
 			this.getRouter().navTo("studentDetails", {
 				Adm: newpath
-				
+
 			});
 		},
 
