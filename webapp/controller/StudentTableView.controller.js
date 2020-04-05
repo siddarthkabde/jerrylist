e6df164edf35a4cc0c176fe3b35f25c045d0c029
siddarthkabde <siddarth.kabde@gmail.com>
2020-04-05 17:51:08 +0000

    V89

--- webapp/controller/StudentTableView.controller.js
+++ webapp/controller/StudentTableView.controller.js
@@ -415,14 +415,35 @@ sap.ui.define([
 				aGroups.push(new Sorter(sPath, bDescending, vGroup));
 				// apply the selected group settings
 				oBinding.sort(aGroups);
-				
-				} else{
-					
+
+			} else {
+
 				oTable.getModel().refresh(true);
+			}
+
+		},
+
+		onPrint: function (oEvent) {
+				var oTarget = this.getView(),
+					sTargetId = oEvent.getSource().data("targetId");
+
+				if (sTargetId) {
+					oTarget = oTarget.byId(sTargetId);
 				}
 
+				if (oTarget) {
+					var $domTarget = oTarget.$()[0],
+						sTargetContent = $domTarget.innerHTML,
+						sOriginalContent = document.body.innerHTML;
+
+					document.body.innerHTML = sTargetContent;
+					window.print();
+					document.body.innerHTML = sOriginalContent;
+				} else {
+					jQuery.sap.log.error("onPrint needs a valid target container [view|data:targetId=\"SID\"]");
+				}
 			}
-		// }
+			// }
 	});
 
 });
