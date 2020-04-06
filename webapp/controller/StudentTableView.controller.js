3a2a717738c3324e947804ec410cbdfb45a5512a
siddarthkabde <siddarth.kabde@gmail.com>
2020-04-06 17:44:03 +0000

    V 10

--- webapp/controller/StudentTableView.controller.js
+++ webapp/controller/StudentTableView.controller.js
@@ -424,24 +424,24 @@ sap.ui.define([
 		},
 
 		onPrint: function (oEvent) {
-				var oTarget = this.getView(),
-					sTargetId = oEvent.getSource().data("targetId");
+				// var oTarget = this.getView(),
+				// 	sTargetId = oEvent.getSource().data("targetId");
 
-				if (sTargetId) {
-					oTarget = oTarget.byId(sTargetId);
-				}
+				// if (sTargetId) {
+				// 	oTarget = oTarget.byId(sTargetId);
+				// }
 
-				if (oTarget) {
-					var $domTarget = oTarget.$()[0],
-						sTargetContent = $domTarget.innerHTML,
-						sOriginalContent = document.body.innerHTML;
+				// if (oTarget) {
+				// 	var $domTarget = oTarget.$()[0],
+				// 		sTargetContent = $domTarget.innerHTML,
+				// 		sOriginalContent = document.body.innerHTML;
 
-					document.body.innerHTML = sTargetContent;
-					window.print();
-					document.body.innerHTML = sOriginalContent;
-				} else {
-					jQuery.sap.log.error("onPrint needs a valid target container [view|data:targetId=\"SID\"]");
-				}
+				// 	document.body.innerHTML = sTargetContent;
+				// 	window.print();
+				// 	document.body.innerHTML = sOriginalContent;
+				// } else {
+				// 	jQuery.sap.log.error("onPrint needs a valid target container [view|data:targetId=\"SID\"]");
+				// }
 			}
 			// }
 	});
